90ceaf6d85b50e74ebcaa516675cc11e0eaa8ac0
ljy2010a <ljy2010a@gmail.com>
2018-05-28 15:58:36 +0800

    Add : more test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:2.7.12
 ADD . .
-ADD . /work2
+ADD . /work
 RUN pip install -r requirements.txt --no-cache-dir
 CMD sh entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,13 @@ echo "=================="
 echo "first test :"
 python test.py
 echo "second test : "
-cd /work2
+cd /work
 python test.py
 echo "third test : "
-cd /work/test_import
+cd /work2/test_import
+python test.py
+echo "fourth test : "
+mkdir -p /work3
+cp -r /work2/test_import /work3
+cd /work3/test_import
 python test.py
