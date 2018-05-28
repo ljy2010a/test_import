66703f551a648df5f295b40405d483288af0170e
ljy2010a <ljy2010a@gmail.com>
2018-05-28 18:04:56 +0800

    Add : more test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,21 @@
 echo "=================="
 echo "first test :"
 python test.py
+
 echo "second test : "
 cd /work
 python test.py
+
 echo "third test : "
 cd /work2/test_import
 python test.py
+
 echo "fourth test : "
 mkdir -p /work3
 cp -r /work2/test_import /work3
 cd /work3/test_import
 python test.py
+
+echo "fifth test : "
+cd /work2/test_import
+python test2.py
---
+++ test2.py
@@ -0,0 +1,3 @@
+from utils2.crypto2 import decode_user_name
+
+print(decode_user_name('username'))
